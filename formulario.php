f09ff84fa68e6ce988ca92b98862e6bd9700f855
EstrellitaC <estrellita.caycedo@tecsup.edu.pe>
2023-04-21 10:38:49 -0500

    otro cambio

--- formulario.php
+++ formulario.php
@@ -1,47 +1,22 @@
 <?php
-/*
-Este archivo lista todos los datos de la tabla, obteniendo a los mismos como un arreglo
-*/
-?>
-<?php
-include_once "base_de_datos.php";
-$sentencia = $base_de_datos->query("select id, nombre, edad from mascotas");
-$mascotas = $sentencia->fetchAll(PDO::FETCH_OBJ);
-?>
 
+?>
 <?php include_once "encabezado.php" ?>
 <div class="row">
-
     <div class="col-12">
-        <h1>Listar con arreglo</h1>
-        <br>
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Edad</th>
-                        <th>Editar</th>
-                        <th>Eliminar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!--
-                    Modificación
-                    -->
-                    <?php foreach($mascotas as $mascota){ ?>
-                        <tr>
-                            <td><?php echo $mascota->id ?></td>
-                            <td><?php echo $mascota->nombre ?></td>
-                            <td><?php echo $mascota->edad ?></td>
-                            <td><a class="btn btn-warning" href="<?php echo "editar.php?id=" . $mascota->id?>">Editar</a></td>
-                            <td><a class="btn btn-danger" href="<?php echo "eliminar.php?id=" . $mascota->id?>">Eliminar</a></td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
+        <h1>Agregar</h1>
+        <form action="insertar.php" method="POST">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input required name="nombre" type="text" id="nombre" placeholder="Nombre de mascota" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="edad">Edad</label>
+                <input required name="edad" type="number" id="edad" placeholder="Edad de mascota" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-succes">Guardar</button>
+            <a href="./listar.php" class="btn btn-warning">Ver todas</a>
+        </form>
     </div>
 </div>
 <?php include_once "pie.php" ?>
